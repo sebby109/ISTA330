480bb907bd4faf634ae7682f9d98c82b66672d03
Sebastian Galindo <sebastianga14@hotmail.com>
2021-02-08 01:03:48 -0700

    finished

--- practicum/week-2/question10.js
+++ practicum/week-2/question10.js
@@ -8,5 +8,22 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
+    var max_val = 0;
+    var min_val = n;
+    var str_num = n.toString();
+
+    for(i=0; i<str_num.length;i++){
+        var temp_int = parseInt(str_num[i])
+        if(temp_int > max_val){
+            max_val = temp_int;
+        }
+        if(temp_int < min_val){
+            min_val = temp_int;
+        }
+    }
+
+    var retval = max_val.toString() + ' - ' + min_val.toString() + ' = ' + (max_val - min_val).toString();
+
+    return retval;
 
 };
