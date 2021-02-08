def29b8de6c6d6074ef630dada67c57670834e6d
Sebastian Galindo <sebastianga14@hotmail.com>
2021-02-07 22:16:43 -0700

    finished

--- practicum/week-2/question1.js
+++ practicum/week-2/question1.js
@@ -5,6 +5,24 @@ For example given the input array [5, 6, 1], the running sum is
 [5, 5+6, 5+6+1]
 */
 
+
+
+
 var runningSum = function(input) {
-   
+   var retval = [];
+
+    if (input.length == 1){
+        return input;
+    }
+
+    for (i=0 ; i < input.length; i++){
+        if (i == 0){
+            retval.push(input[i])
+        }
+        else{
+            retval.push(retval[i-1] + input[i])
+        }
+    }
+
+    return retval;
 };
