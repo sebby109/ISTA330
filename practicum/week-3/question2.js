b15d89b77ce42e4f4c9ff069b9dfa0e082c63994
Sebastian Galindo <sebastianga14@hotmail.com>
2021-02-22 19:08:07 -0700

    Finished

--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -10,6 +10,22 @@ Example: input: [-12,3,-1,5,-2,1,-7]
           */
 
  var largestSubarray = function(input) {
-     
+        current_max = input[0];
+        retval = input[0];
+
+        for (i=1; i < input.length; i++){
+                if ((current_max + input[i]) > input[i]){
+                        current_max = (current_max + input[i]);
+                }
+                else if (input[i] > current_max){
+                        current_max = input[i];
+                }
+
+                if (current_max > retval){
+                        retval = current_max;
+                }
+        }
+
+        return retval;
     
  };
